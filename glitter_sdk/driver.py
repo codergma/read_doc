4ec08b3c2049304e7594e629af88f5d8e2688033
eliubin <eliubin@didichuxing.com>
2022-04-01 23:15:19 +0800

    update

--- glitter_sdk/driver.py
+++ glitter_sdk/driver.py
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 # Code is Apache-2.0 and docs are CC-BY-4.0
 
-from transport import Transport
-from utils import normalize_nodes
+from GlitterClient.transport import Transport
+from GlitterClient.utils import normalize_nodes
 
 class GlitterClient:
     """A :class: `driver.GlitterClient` is python client  for glitter.
